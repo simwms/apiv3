bdfb6266e21b5fc1b94c47f2673b0fc4f1861bf8
Thomas Chen <foxnewsnetwork@gmail.com>
2015-09-04 22:54:08 -0700

    dklsjfasdf

--- lib/apiv3/plugs/master_key.ex
+++ lib/apiv3/plugs/master_key.ex
@@ -1,6 +1,7 @@
 defmodule Apiv3.Plugs.MasterKey do
   import Plug.Conn
   import Phoenix.Controller, only: [render: 4]
+  @master_key Application.get_env(:simwms, :master_key)
 
   def init(o), do: o
 
@@ -15,13 +16,15 @@ defmodule Apiv3.Plugs.MasterKey do
     end
   end
 
-  @master_key Application.get_env(:simwms, :master_key)
   defp has_master_key?(conn) do
     conn 
     |> get_req_header("simwms-master-key")
     |> List.first
-    |> equal?(@master_key)
+    |> match_master_key?
   end
 
-  defp equal?(a,b), do: String.strip(a) == String.strip(b)
+  defp match_master_key?(a) when is_binary(a) do
+    String.strip(a) == @master_key
+  end
+  defp match_master_key?(_), do: false
 end
